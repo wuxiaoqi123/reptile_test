630bbcd4c5215b788d824e791bf80ed5e724d1dc
武小奇 <1321972760@qq.com>
2019-01-30 22:48:04 +0800

    下载图片

--- budejie/Test1.py
+++ budejie/Test1.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 import requests
 from pyquery import PyQuery as pq
+import os
 
 url = 'http://www.budejie.com/'
 
@@ -36,8 +37,21 @@ def parse_page(html):
                'img': item.attr('data-original')}
 
 
+def download(filename, img_url):
+    path = 'budejie'
+    isExists = os.path.exists(path)
+    if not isExists:
+        os.makedirs(path)
+    split = img_url.split('.')
+    if split[-1] == 'jpg' or split[-1] == 'gif':
+        filename = filename.strip().replace('\n', '') + '.' + split[-1]
+        response = requests.get(img_url)
+        with open(path + "/" + filename, 'wb') as f:
+            f.write(response.content)
+
+
 if __name__ == '__main__':
     html = get_page(1)
     data = parse_page(html)
     for d in data:
-        print(d)
+        download(d['title'], d['img'])
